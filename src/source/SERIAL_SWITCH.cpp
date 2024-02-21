7731942a57b870cdc1f3c7658d6cab0de3e679af
elektroman93 <skorkaoliver93@gmail.com>
2024-02-21 12:53:45 +0100

    Update SERIAL_SWITCH.cpp

--- src/source/SERIAL_SWITCH.cpp
+++ src/source/SERIAL_SWITCH.cpp
@@ -7,15 +7,15 @@ SSwitch::SSwitch( const uint16_t pin1, const uint16_t pin2, const uint16_t pin3,
   p4(pin4),
   addr(address)
 	{
-		pinMode(pin1, OUTPUT);		
-    pinMode(pin2, OUTPUT);
-		pinMode(pin3, OUTPUT);		
-    pinMode(pin4, OUTPUT);
+	pinMode(pin1, OUTPUT);		
+    	pinMode(pin2, OUTPUT);
+	pinMode(pin3, OUTPUT);		
+    	pinMode(pin4, OUTPUT);
 	}
 
 void SSwitch::active() {
   digitalWrite(p1, addr & 0b0001 ? HIGH : LOW);
   digitalWrite(p2, addr & 0b0010 ? HIGH : LOW);
-  digitalWrite(p1, addr & 0b0100 ? HIGH : LOW);
-  digitalWrite(p2, addr & 0b1000 ? HIGH : LOW);
+  digitalWrite(p3, addr & 0b0100 ? HIGH : LOW);
+  digitalWrite(p4, addr & 0b1000 ? HIGH : LOW);
 }
